6c951f62506aff2b1009f40af7c06098315993a5
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-06-06 23:26:51 +0100

    storing fetch data in a stack

--- randomQuotes/src/App.jsx
+++ randomQuotes/src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css'
+import ListQuotes from './components/ListQuotes';
+import QuoteItem from './components/QuoteItem';
 import useFetchData from './customHooks/useFetchData'
 
 function App() {
-  const { data, isLoading, error} = useFetchData("https://api.quotable.io/random")
+  const { data, isLoading, error} = useFetchData("https://api.quotable.io/random");
+  const [currentQuote,...storedData] = data;
+  const listContent = storedData.map(item => {
+    return(
+      <>
+        <QuoteItem {...item}/>
+      </>
+    )
+  })
   if(error){
     return(
       <>
@@ -18,11 +28,17 @@ function App() {
         <h1>Random Quotes</h1>
       </header>
       <main>
-        {isLoading ? <h1>Loading....</h1>:<div>
-          <h1>{data.content}</h1>
-          <h2>Author:{data.author}</h2>
+        {isLoading ? <h1>Loading....</h1>:
+        <div>
+          
+              <h1>{currentQuote.content}</h1>
+              <h2>Author:{currentQuote.author}</h2>
         </div>}
-      
+        <div>
+          <ListQuotes>
+            {listContent}
+          </ListQuotes>
+        </div>
       </main>
       <footer></footer>
     </>
